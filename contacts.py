c3f585dcd3b9cf5ade6f0f52917342396162e804
alperencubuk <alperencubuk@gmail.com>
2019-07-28 20:35:41 +0300

    Added input_number function

--- contacts.py
+++ contacts.py
@@ -1,34 +1,36 @@
-#Dictionary Exercise
+# Dictionary Exercise (Basic Contacts App)
+
+from os import system
 
 def add_contact(contacts, name, number):
     if name in contacts.keys():
         print(name, 'kisisi zaten var.')
     else:
         contacts.update({name: number})
-        print(name,':', number, 'rehbere eklendi')
+        print(name,':', number, 'rehbere eklendi.')
 
 
 def delete_contact(contacts, name):
     try:
         contacts.pop(name)
-        print(name, 'rehberden silindi')
+        print(name, 'rehberden silindi.')
     except KeyError:
-        print('Kayit Bulunamadi.')
+        print('Kayit bulunamadi.')
 
 
 def edit_contact(contacts, name, number):
     try:
         contacts[name]=number
-        print(name,'in numarasi ', number, 'olarak guncellendi')
+        print(name,'in numarasi ', number, 'olarak guncellendi.')
     except KeyError:
-        print('Kayit Bulunamadi.')
+        print('Kayit bulunamadi.')
 
 
 def find_contact(contacts, name):
     try:
         print(name, ': ', contacts[name])
     except KeyError:
-        print('Kayit Bulunamadi.')
+        print('Kayit bulunamadi.')
 
 
 def print_all_contacts(contacts):
@@ -45,33 +47,59 @@ Lutfen Yapmak Istediginiz Islemi Secin:
     3) Kayit Sil
     4) Kayit Duzenle
     5) Rehberde Bul
-    6) Cikis''')
+    6) Cikis\n''')
+
+
+def input_number(message):
+    while True:
+        try:
+            number=int(input(message))
+        except ValueError:
+            print('Numara rakamlardan olusmalidir.')
+            continue
+        break
+    return number
 
 
 contacts={} #dict {name: number}
+system('clear')
 print_main_menu()
 while(True):
     try:
-        choice=int(input('Seciminiz: '))
+        choice=int(input('\nSeciminiz: '))
     except ValueError:
-        print('Hatali Giris Yaptiniz. Tekrar Deneyin.')
+        print('Hatali giris yaptiniz. Tekrar deneyin.')
         continue
     if choice==1:
+        system('clear')
+        print_main_menu()
         print_all_contacts(contacts)
     elif choice==2:
         name=input('Isim Girin: ')
-        number=int(input('Numara Girin: '))
+        number=input_number('Numara Girin: ')
+        system('clear')
+        print_main_menu()
         add_contact(contacts, name, number)
     elif choice==3:
         name=input('Silinmesini Istediginiz Kisinin Ismini Girin: ')
+        system('clear')
+        print_main_menu()
         delete_contact(contacts, name)
     elif choice==4:
         name=input('Duzenlenecek Kisinin Ismini Girin: ')
-        number=int(input('Kisinin Yeni Numarasini Girin: '))
+        number=input_number('Kisinin Yeni Numarasini Girin: ')
+        system('clear')
+        print_main_menu()
         edit_contact(contacts, name, number)
     elif choice==5:
         name=input('Bulmak Istediginiz Kisinin Ismini Girin: ')
+        system('clear')
+        print_main_menu()
         find_contact(contacts, name)
     else:
+        system('clear')
+        print_main_menu()
         print('Cikis Yapildi.')
         break
+
+# Alperen Cubuk
