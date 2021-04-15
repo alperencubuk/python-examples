0c33e17c90bcf5dff7e2c3634d8835632b5effa4
alperencubuk <alperencubuk@outlook.com>
2021-04-15 19:40:06 +0300

    Added sms

---
+++ sms.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import requests
+from os import system as s
+
+sor = input("Tel adresi Örn: 905555555555 >>> ")
+
+mesaj = input("Mesajınız >>> ")
+
+arlk = mesaj[0:70]
+
+print("\n| Mesajınızın Gönderilebilecek kısmı aşagıdaki gibidir.\n"+arlk)
+
+drlm = input("\n| Mesajınız Gönderilsinmi?[y/n] >>> ")
+
+if drlm == "y" or drlm == "Y":
+    print("\n"+sor+"\n"+arlk+"\n")
+    resp = requests.post('https://textbelt.com/text', {
+  'phone': sor,
+  'message': arlk,
+  'key': 'textbelt',
+    })
+    print(resp.json())
+
+elif drlm == "n" or drlm == "N":
+    quit()
+else:
+    print("\n|Hata yaptınız.")
